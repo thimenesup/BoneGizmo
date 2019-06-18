10ac897ade6edcae004684cc52da404999577a3e
PrestonKnopp <prestonknopp@gmail.com>
2019-06-17 20:23:00 -0700

    rm unused bone_gizmo.gd

--- addons/bone_gizmo/bone_gizmo.gd
+++
@@ -1,8 +0,0 @@
-tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("BoneGizmo", "Spatial", preload("bone_gizmo_node.gd"), preload("bone_gizmo.png"))
-
-func _exit_tree():
-	remove_custom_type("BoneGizmo")
