dec26056fd66b287650a073cd76716b765c0599a
PrestonKnopp <prestonknopp@gmail.com>
2019-06-17 20:41:49 -0700

    rename bone_gizmo_node to bone_gizmo

--- addons/bone_gizmo/plugin.gd
+++ addons/bone_gizmo/plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 var dock
 
 func _enter_tree():
-	add_custom_type("BoneGizmo", "Spatial", preload("bone_gizmo_node.gd"), preload("icon.png"))
+	add_custom_type("BoneGizmo", "Spatial", preload("bone_gizmo.gd"), preload("icon.png"))
 	dock = preload("res://addons/bone_gizmo/dock.tscn").instance()
 	add_control_to_dock( DOCK_SLOT_LEFT_UL, dock)
 
